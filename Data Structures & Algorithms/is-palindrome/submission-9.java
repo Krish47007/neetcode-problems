48183d5f9684134a8722c5833560e86bde14db54
Krish47007 <krishnendu.gupta00@gmail.com>
2026-03-30 14:01:06 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public boolean isPalindrome(String s) {
+
+        char[] str = s.toCharArray();
+        int beg = 0, end = str.length - 1;
+
+        while(beg < end)
+        {
+            //Ignore Non-alphanumeric chars
+            if(!Character.isLetterOrDigit(str[beg]))
+                beg++;
+            else if(!Character.isLetterOrDigit(str[end]))
+                end--;
+            else
+            {
+                if(Character.toLowerCase(str[beg]) != Character.toLowerCase(str[end]))
+                    return false;
+                
+                beg++;
+                end--;
+            }
+        }
+
+        return true;
+        
+    }
+}
