1c7109e13d4b070e5a5e92e2c3291ba45a2ac10f
Krish47007 <krishnendu.gupta00@gmail.com>
2026-04-04 22:12:58 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    private Map<Character,Character> map = Map.of(')','(', '}','{',']','[');
+
+    public boolean isValid(String s) {
+        
+        Deque<Character> stk = new ArrayDeque<>();
+        char[] str = s.toCharArray();
+        
+        for(char ch : str)
+        {
+            //Push the opposite pair in Stack.
+            if(ch == '(')
+                stk.push(')');
+            else if(ch == '{')
+                stk.push('}');
+            else if(ch == '[')
+                stk.push(']');
+            else
+            {
+                //Meaning either we got the complement pair first 
+                //or top of stack is not same as complement pair
+                if(stk.isEmpty() || stk.peek() != ch)
+                    return false;
+                
+                stk.pop();
+            }
+        }
+
+        return stk.isEmpty();
+    }
+}
