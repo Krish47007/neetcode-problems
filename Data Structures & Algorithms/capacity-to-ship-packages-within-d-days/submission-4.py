34bb46b29449e48e63bbfc9f1f31d6bb76c6c4d0
Krish47007 <krishnendu.gupta00@gmail.com>
2026-04-10 13:46:42 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-4

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def canShip(self,weights: List[int],cap: int, totalShips: int) -> bool:
+
+        shipCount,currentCap = 1,cap
+        for i in range(0,len(weights)):
+            if currentCap - weights[i] < 0:
+                shipCount += 1
+                if shipCount > totalShips:
+                    return False
+                currentCap = cap
+            currentCap -= weights[i]
+        
+        return True
+
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        low,high,n = float('-inf'),0,len(weights)
+
+        for i in range(0,n):
+            low = max(low,weights[i])
+            high += weights[i]
+        
+        ans = high
+
+        while low <= high:
+            cap = low + (high - low) // 2
+            if self.canShip(weights,cap,days):
+                ans = min(ans,cap)
+                high = cap - 1
+            else:
+                low = cap + 1
+
+        return ans
