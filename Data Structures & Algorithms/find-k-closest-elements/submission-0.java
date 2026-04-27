5405db1f33ae8f43592c2dc4164d0039a0bd7b3c
Krish47007 <krishnendu.gupta00@gmail.com>
2026-04-27 11:44:01 +0530

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        //Convert the array into Integer[] as sort takes Integer[] for comparator
+        Integer[] nums = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        int n = arr.length;
+        int[] ans = new int[n];
+        //Sort the array based on absolute value of x - a[i]
+        Arrays.sort(nums,(a,b) -> {
+            int diff = Integer.compare(Math.abs(x - a),Math.abs(x - b));
+            if(diff == 0)
+                return Integer.compare(a,b);
+            return diff;
+        });
+
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0;i<k;i++)
+            res.add(nums[i]);
+        
+        Collections.sort(res);
+        return res;
+    }
+}
